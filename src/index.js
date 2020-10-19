5c26e7662b0c781adbc17df4d51e9d99b0880c7e
jplopez3 <jp-lopez@hotmail.com>
2020-10-19 13:13:19 +0200

    Add start node server

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({hello: 'world'});
+    res.json( process.env
+    );
 });
+
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(`/api`, router);
+//express()
+ // .use(express.static(path.join(__dirname, 'public')))
+ // .set('views', path.join(__dirname, 'views'))
+ // .set('view engine', 'ejs')
+  //.get('/', (req, res) => res.render('pages/index'))
+  
