4e470840201b363a98f66d8b2dfec44f129b63df
jplopez7 <jp-lopez@hotmail.com>
2021-08-17 11:17:24 +0100

    Update cors options

--- src/loaders/app.js
+++ src/loaders/app.js
@@ -1,21 +1,28 @@
 import express, { Router } from 'express';
-//import path from 'path';
+import exphbs from 'express-handlebars';
+import path from 'path';
 import compression from 'compression';
 import cors from 'cors';
+import corsOptions from './cors.js';
+//import bodyParser from 'bodyParser';
 import morgan from './morgan.js';
 import {
+	outscore,
 	fixtures,
 	players,
-	outscore,
 	standings,
 	teams,
 	betshelper,
 	injuries,
 } from '../resources/index.js';
+import {
+	notFoundError,
+	serverError,
+} from '../utils/middlewares/errors/index.js';
 import Logger from './winston.js';
 import { apiPath } from '../config/constants.js';
 
-// const __dirname = path.resolve( path.dirname( '' ) ) + '/src/';
+const __dirname = path.resolve(path.dirname('')) + '/src/';
 
 export default class App {
 	constructor() {
@@ -28,18 +35,18 @@ export default class App {
 		}
 	}
 	initMiddleware() {
-		// general config
-		// this.app.set('views', path.join(__dirname, 'views'));
-		//this.app.set('view engine', 'html');
-		// this.app.use(express.static(path.join(__dirname, 'public')))
+		// View engine config
+		this.app.set('views', path.join(__dirname, 'views'));
+		this.app.engine('handlebars', exphbs());
+		this.app.set('view engine', 'handlebars');
+		this.app.use(express.static(path.join(__dirname, 'public/')));
 
-		// Use Morgan middle ware
+		// Use Morgan logger middle ware
 		this.app.use(morgan);
 
+		//enable verbose errors
 		this.app.enable('verbose errors');
-
 		// disable them in production
-		// use $ NODE_ENV=production node examples/error-pages
 		if (this.app.settings.env === 'production') {
 			this.app.disable('verbose errors');
 		}
@@ -48,14 +55,15 @@ export default class App {
 		// Alternate description:
 		// Enable Cross Origin Resource Sharing to all origins by default
 		this.app.use(cors());
+
 		// Useful if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 		// It shows the real origin IP in the heroku or Cloudwatch logs
 		this.app.enable('trust proxy');
+
 		this.app.disable('x-powered-by');
 		// Middleware that transforms the raw string of req.body into json
-		//server.use(bodyParser.json());
+		//this.app.use(bodyParser.json());
 		this.app.use(compression());
-		/* app.set('view engine', 'html');*/
 	}
 	initRoutes(apiPath = '/') {
 		Logger.warn('Initializing endpoints in %s', apiPath);
@@ -69,34 +77,14 @@ export default class App {
 		this.ApiV3Router.use('/injuries', injuries);
 		this.initErrorHandling(this.ApiV3Router);
 		this.app.use(apiPath, this.ApiV3Router);
+		this.app.use('/', outscore);
 		this.initErrorHandling(this.app);
 	}
 
 	initErrorHandling(route) {
-		//Todo: move logic to middlewares
 		// Error handlers
-		route.use(function (req, res, next) {
-			Logger.warn('Resource not found %s', req.path);
-			res.status(404);
-			res.json({ error: 'Resource not found' });
-		});
-
-		route.use(function (err, req, res, next) {
-			// we may use properties of the error object
-			// here and next(err) appropriately, or if
-			// we possibly recovered from the error, simply next().
-			Logger.error(
-				'ErrorHandling Middleware Status: %s - Error: %o',
-				err.status,
-				err
-			);
-			res.status(500);
-			if (err?.type === 'rapidApi') {
-				res.json({ error: err.error });
-			} else {
-				res.json({ error: 'Something failed!' });
-			}
-		});
 		Logger.info('initializing error handling');
+		route.use(notFoundError);
+		route.use(serverError);
 	}
 }
